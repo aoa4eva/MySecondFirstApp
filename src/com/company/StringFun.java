65acf52b5e97dd54e4951d74a5b78d768167da80
aoa4eva <afua.ankomah@gmail.com>
2018-10-03 16:54:18 -0400

    Updated my files

--- src/com/company/StringFun.java
+++ src/com/company/StringFun.java
@@ -4,7 +4,6 @@ public class StringFun {
     public static void main(String[] args) {
         String myName = "Josemy";
         System.out.println(myName.isEmpty()?"This string is empty":"This string is not empty");
-        System.out.println(myName);
-
+        System.out.println("My name is "+myName);
     }
 }
